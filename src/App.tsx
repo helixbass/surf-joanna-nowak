de4fc60b6022319ba1e5e71c65789bc79ed24bf9
Julian Rosse <julian@helixbass.net>
2020-06-16 20:32:51 -0400

    <BlueStripe>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
 import React, {FC} from 'react'
-import {flowMax, addDisplayName} from 'ad-hok'
+import {flowMax, addDisplayName, addProps} from 'ad-hok'
 
 import {makeStyles, colors} from 'utils/style'
 
 const CIRCLE_WIDTH = 400
+const CIRCLE_RADIUS = CIRCLE_WIDTH * 0.5
 const INNER_CIRClE_CLIP_PATH_ID = 'inner-circle-clip-path'
 const INNER_CIRCLE_PADDING_PER_SIDE = CIRCLE_WIDTH * 0.068
 const INNER_CIRCLE_WIDTH = CIRCLE_WIDTH - 2 * INNER_CIRCLE_PADDING_PER_SIDE
+// const INNER_CIRCLE_RADIUS = INNER_CIRCLE_WIDTH * 0.5
+const {PI} = Math
+
+interface BlueStripeProps {
+  startPosition: number
+  endPosition: number
+}
+
+const BlueStripe: FC<BlueStripeProps> = flowMax(
+  addDisplayName('BlueStripe'),
+  addProps(
+    flowMax(
+      ({startPosition, endPosition}) => ({
+        startX: Math.sin(startPosition),
+        startY: Math.cos(startPosition) * -1,
+        endX: Math.sin(endPosition),
+        endY: Math.cos(endPosition) * -1,
+      }),
+      ({startX, startY, endX, endY}) => ({
+        startX: CIRCLE_RADIUS + startX * CIRCLE_RADIUS,
+        startY: CIRCLE_RADIUS + startY * CIRCLE_RADIUS,
+        endX: CIRCLE_RADIUS + endX * CIRCLE_RADIUS,
+        endY: CIRCLE_RADIUS + endY * CIRCLE_RADIUS,
+      }),
+    ),
+  ),
+  ({startX, startY, endX, endY}) => (
+    <path
+      d={`M ${startX} ${startY} L ${endX} ${endY}`}
+      stroke={colors.blueRoad}
+      strokeWidth={CIRCLE_WIDTH * 0.14}
+    />
+  ),
+)
 
 const App: FC = flowMax(addDisplayName('App'), () => (
   <div css={styles.container}>
@@ -31,11 +66,7 @@ const App: FC = flowMax(addDisplayName('App'), () => (
         fill={colors.white}
       />
       <g clipPath={`url(#${INNER_CIRClE_CLIP_PATH_ID})`}>
-        <path
-          d={`M ${CIRCLE_WIDTH} 0 L 0 ${CIRCLE_WIDTH * 0.3}`}
-          stroke={colors.blueRoad}
-          strokeWidth={CIRCLE_WIDTH * 0.14}
-        />
+        <BlueStripe startPosition={PI * 0.17} endPosition={PI * 1.7} />
       </g>
     </svg>
   </div>
---
+++ src/utils/fp.ts
@@ -0,0 +1,15 @@
+import {UnchangedProps} from 'ad-hok'
+
+type TapType = <TProps>(
+  callback: (props: TProps) => void
+) => UnchangedProps<TProps>
+
+export const tap: TapType = (callback) => (props) => {
+  callback(props)
+
+  return props
+}
+
+type LogType = <TProps>(key: string) => UnchangedProps<TProps>
+
+export const log: LogType = (key) => tap((props) => console.log({[key]: props}))
