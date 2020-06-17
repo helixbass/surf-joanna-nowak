7641c32a5a3749f1e8812aae8ee59e3c38cedfca
Julian Rosse <julian@helixbass.net>
2020-06-16 20:10:47 -0400

    blue stripe

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import {flowMax, addDisplayName} from 'ad-hok'
 import {makeStyles, colors} from 'utils/style'
 
 const CIRCLE_WIDTH = 400
+const INNER_CIRClE_CLIP_PATH_ID = 'inner-circle-clip-path'
+const INNER_CIRCLE_PADDING_PER_SIDE = CIRCLE_WIDTH * 0.068
+const INNER_CIRCLE_WIDTH = CIRCLE_WIDTH - 2 * INNER_CIRCLE_PADDING_PER_SIDE
 
 const App: FC = flowMax(addDisplayName('App'), () => (
   <div css={styles.container}>
@@ -12,12 +15,28 @@ const App: FC = flowMax(addDisplayName('App'), () => (
       width={CIRCLE_WIDTH}
       viewBox={`0 0 ${CIRCLE_WIDTH} ${CIRCLE_WIDTH}`}
     >
+      <defs>
+        <clipPath id={INNER_CIRClE_CLIP_PATH_ID}>
+          <circle
+            cx={CIRCLE_WIDTH / 2}
+            cy={CIRCLE_WIDTH / 2}
+            r={INNER_CIRCLE_WIDTH / 2}
+          />
+        </clipPath>
+      </defs>
       <circle
         cx={CIRCLE_WIDTH / 2}
         cy={CIRCLE_WIDTH / 2}
         r={CIRCLE_WIDTH / 2}
         fill={colors.white}
       />
+      <g clipPath={`url(#${INNER_CIRClE_CLIP_PATH_ID})`}>
+        <path
+          d={`M ${CIRCLE_WIDTH} 0 L 0 ${CIRCLE_WIDTH * 0.3}`}
+          stroke={colors.blueRoad}
+          strokeWidth={CIRCLE_WIDTH * 0.14}
+        />
+      </g>
     </svg>
   </div>
 ))
--- src/utils/style.ts
+++ src/utils/style.ts
@@ -10,4 +10,5 @@ export const makeStyles = <TStyles extends StylesObject>(styles: TStyles) =>
 export const colors = {
   blueBackground: '#d2dfd9',
   white: '#fcf9e9',
+  blueRoad: '#21325f',
 }
